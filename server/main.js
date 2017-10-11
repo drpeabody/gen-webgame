bba119cb8e273b00344fd39baf1005468ee6843b
drpeabody <abhiagar97@gmail.com>
2017-10-11 22:13:49 +0530

    modified:   lib/def.js 	modified:   server/main.js 	deleted:    todo

--- server/main.js
+++ server/main.js
@@ -16,11 +16,10 @@ players = [];//Important: players is sorted according to the 'id' field of the a
 //Server states:
 	//0 - Logged in palying it cool
 	//1 - Looking for Game
-	//2 - Game found, allotted a room
+	//2 - Game found, alloting a room
 	//3 - Playing
 //Client Login - Add him to the list of players - state set to 0
-//Client requestGame() - set state to 1 - look for rooms - if found, add him to the room and return room key - if not found, return "TimeOut" state identifier
-//Client readyForGame() - set state to 2 - Cross check for a valid room allotment - initialise game
+//Client requestGame() - set state to 1 - look for rooms - if found, add him to the room, perform final checks and begin game - if not found, return "TimeOut" state identifier
 
 console.log("server started");
 
@@ -32,7 +31,7 @@ Meteor.methods({
 	},
 	signin: function(name, pwd) {
 		if(!authenticateCreds(name, pwd)) return ["Incorrect username/password", 'guest', 0];
-		//Add Player checks for already signed in cases automatically
+		//addPlayer() checks for already signed in cases automatically
 		if(addPlayer({id: name, state: 0, notif: ''})) return ["Login Successful", name, playerList.findOne({username: name, pass: pwd}).score];
 		else return ["Login failed", 'guest', 0];
 	},
@@ -46,7 +45,29 @@ Meteor.methods({
 		}
 	},
 	requestGame: function(name, pwd){
-		//Todo
+		if(!authenticateCreds(name,pwd)) return;
+		var v = findSupposedPosOfPlayer(name);
+		if(v.found){ if(players[v.idx].state == 0) players[v.idx].state = 1; else console.log("Invalid state for requestGame() by " + name); }
+		pl = getWaitingPlayer();
+		if(pl == undefined) return;
+		
+		pl.filter((x) => x.state == 1);
+		
+		if(pl.length < COUNT_MIN_PLAYERS || pl.length > COUNT_MAX_PLAYERS){
+			pl.forEach((x) => x.state = 1);
+			return;
+		}
+		else {
+			var key = hash(JSON.parse(pl));
+			pl.forEach((x) => {
+				x.state = 2;
+				x.notif = key;	
+			});
+		}
+		
+		//Host Room
+		//Set all states to 3
+		
 	},
 	sendMessageToRoom: function(key, author, message){
 		//Todo
@@ -56,17 +77,14 @@ Meteor.methods({
 		var v = findSupposedPosOfPlayer(name);
 		if(v.found) return players[v.idx].notif;
 		else return undefined;
-	},
-	readyForGame: function(roomKey){
-		
-		//Check if all players in the room are ready
-		//If they are, begin game
 	}
 	//Stat before and stat after
-	//Prep for the multiplayer thing
 });
 
-function getWaitingPlayer(){
+//For the most general case we might want to get more than one player
+//Return undefined if no player is found
+//Return Array of references to the player objects in the global
+function getWaitingPlayer(num){
 	//MatchMaking
 }	
 
