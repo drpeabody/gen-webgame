658e9b88e5387b2eaeed0915cb9780bfe1d88a94
drpeabody <abhiagar97@gmail.com>
2024-02-04 13:10:53 +0530

    Added all the songs from draft tracklist

---
+++ client-web-server/TheMusicOfLinkinPark/resources/heavy/index.js
@@ -0,0 +1,40 @@
+
+
+const songHeavy = new Song(
+    './resources/heavy/06Heavy.mp3', 
+
+    // Animation Manager - All the animated drawings + animation cues for this song
+
+    new AnimationManager([
+
+        // All animation cues
+    ], 
+    
+    // All animated Drawings
+    {
+        
+    })
+    , 
+    // All Static Drawings
+    { 
+        "logo":  new StaticDrawing((ctx, drawing) => {
+            ctx.drawImage(drawing.image, drawing.X, drawing.Y, drawing.width, drawing.height);
+        }, (logo) => {
+            let bannerDimension = Math.min(WIDTH/2, HEIGHT/2);
+            logo.image = new Image();
+            logo.X = WIDTH/2 - bannerDimension/2 - 140;
+            logo.Y = HEIGHT/2 - bannerDimension/2;
+            logo.width = bannerDimension+140;
+            logo.height = bannerDimension;
+            logo.image.onload = () => { logo.enabled = true; }
+            logo.image.src = "./resources/common/linkin-park-logo.svg";
+        })
+    
+    }
+);
+
+// Register this song
+allSongs.push(songHeavy);
+
+// Load the song audio into memory
+songHeavy.load();
---
+++ client-web-server/TheMusicOfLinkinPark/resources/in-the-end/index.js
@@ -0,0 +1,40 @@
+
+
+const songInTheEnd = new Song(
+    './resources/in-the-end/07InTheEnd.mp3', 
+
+    // Animation Manager - All the animated drawings + animation cues for this song
+
+    new AnimationManager([
+
+        // All animation cues
+    ], 
+    
+    // All animated Drawings
+    {
+        
+    })
+    , 
+    // All Static Drawings
+    { 
+        "logo":  new StaticDrawing((ctx, drawing) => {
+            ctx.drawImage(drawing.image, drawing.X, drawing.Y, drawing.width, drawing.height);
+        }, (logo) => {
+            let bannerDimension = Math.min(WIDTH/2, HEIGHT/2);
+            logo.image = new Image();
+            logo.X = WIDTH/2 - bannerDimension/2 - 140;
+            logo.Y = HEIGHT/2 - bannerDimension/2;
+            logo.width = bannerDimension+140;
+            logo.height = bannerDimension;
+            logo.image.onload = () => { logo.enabled = true; }
+            logo.image.src = "./resources/common/linkin-park-logo.svg";
+        })
+    
+    }
+);
+
+// Register this song
+allSongs.push(songInTheEnd);
+
+// Load the song audio into memory
+songInTheEnd.load();
